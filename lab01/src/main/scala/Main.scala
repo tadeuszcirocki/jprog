bc823f8247206542d6e27b1851e84a7c964209aa
Tadeusz Cirocki <tcirocki@linux-p39-4.inf.ug.edu.pl>
2019-02-19 09:40:24 +0100

    l pierwsza commit

---
+++ lab01/src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+object Main extends App {
+  print("podaj liczbe:")
+  var liczba = io.StdIn.readInt()
+  var i = 2
+  if (liczba<2)
+  println("nie jest pierwsza")
+  else{
+  while (i*i<=liczba){
+    if(liczba%i==0){
+    println("nie jest pierwsza")
+    }
+    else
+    println("jest")
+    i=i+1
+  }
+  }
+}
